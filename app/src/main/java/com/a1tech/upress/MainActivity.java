8579dbb1d126ba863220519265e3508310cbcea5
dckrOff <azamatotaboev7621@gmail.com>
2022-04-08 16:56:43 +0500

    added logo and webview

--- app/src/main/java/com/a1tech/upress/MainActivity.java
+++ app/src/main/java/com/a1tech/upress/MainActivity.java
@@ -3,6 +3,7 @@ package com.a1tech.upress;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://www.algosoft.uz/");
     }
 }
