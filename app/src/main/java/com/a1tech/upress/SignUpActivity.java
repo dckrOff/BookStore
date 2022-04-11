3354a4c45cffd1fcd3e9edf6b2da1d41b834df33
dckrOff <azamatotaboev7621@gmail.com>
2022-04-11 13:05:08 +0500

    added sign up activity design

---
+++ app/src/main/java/com/a1tech/upress/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.a1tech.upress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
