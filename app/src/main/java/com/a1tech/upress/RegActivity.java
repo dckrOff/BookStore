eef4b021ae19ef95b3c1cdf21ab65efa84070cf2
dckrOff <azamatotaboev7621@gmail.com>
2022-04-11 15:48:23 +0500

    added sign in page design

--- app/src/main/java/com/a1tech/upress/RegActivity.java
+++ app/src/main/java/com/a1tech/upress/RegActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignInActivity extends AppCompatActivity {
+public class RegActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
