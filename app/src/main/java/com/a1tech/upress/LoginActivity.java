f5211d4d7308fe336fcbe23481fe0155639dda78
dckrOff <azamatotaboev7621@gmail.com>
2022-04-11 17:20:04 +0500

    written, though not completely, but the logic in the LoginActivity and RegActivity classes

--- app/src/main/java/com/a1tech/upress/LoginActivity.java
+++ app/src/main/java/com/a1tech/upress/LoginActivity.java
@@ -2,14 +2,55 @@ package com.a1tech.upress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edMail, edPassword;
+    private Button signInButton;
+    private CheckBox rememberMe;
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
 
+        initViews();
+        buttons();
+    }
+
+    private void initViews() {
+        edMail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPassword);
+        signInButton = findViewById(R.id.signUpButton);
+        rememberMe = findViewById(R.id.checkBox);
+        tvSignUp = findViewById(R.id.tvSignUpBtn);
+    }
+
+    private void buttons() {
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!edMail.getText().toString().trim().isEmpty() && !edPassword.getText().toString().trim().isEmpty()) {
+                    // if edMail == regMail && edPassword == regPass -> Login Succesfull
+                    // if checkbox == true save to sharedPrefs
+                }
+            }
+        });
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/a1tech/upress/RegActivity.java
+++ app/src/main/java/com/a1tech/upress/RegActivity.java
@@ -2,13 +2,70 @@ package com.a1tech.upress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegActivity extends AppCompatActivity {
+
+    private EditText edName, edMail, edPassword, edConfirmPass;
+    private Button signUpBtn;
+    private CheckBox checkBoxTerms;
+    private TextView tvLogInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
+
+        initViews();
+        buttons();
+    }
+
+    private void initViews() {
+        edName = findViewById(R.id.edName);
+        edMail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPassword);
+        edConfirmPass = findViewById(R.id.edConfirmPass);
+        signUpBtn = findViewById(R.id.signUpButton);
+        checkBoxTerms = findViewById(R.id.checkBox);
+        tvLogInBtn = findViewById(R.id.tvSignUpBtn);
+    }
+
+    private void buttons() {
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!edName.getText().toString().trim().isEmpty() && !edMail.getText().toString().trim().isEmpty() && !edPassword.getText().toString().trim().isEmpty()) {
+                    if (edPassword.getText().toString().trim().equals(edConfirmPass.getText().toString().trim())) {
+                        if (checkBoxTerms.isChecked()) {
+                            Intent intent = new Intent(RegActivity.this, MainActivity.class);
+                            // need to save data to sharedPrefs!!!
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(RegActivity.this, "You did not agree to the terms of the user agreement!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(RegActivity.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(RegActivity.this, "Please fill the form!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        tvLogInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
