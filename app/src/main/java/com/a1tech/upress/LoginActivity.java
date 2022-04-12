8d58312362db05d93c683890d8a905ee998576c2
dckrOff <azamatotaboev7621@gmail.com>
2022-04-12 12:29:42 +0500

    removed background statusBar, transperent status bar

--- app/src/main/java/com/a1tech/upress/LoginActivity.java
+++ app/src/main/java/com/a1tech/upress/LoginActivity.java
@@ -3,8 +3,11 @@ package com.a1tech.upress;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -24,6 +27,15 @@ public class LoginActivity extends AppCompatActivity {
 
         initViews();
         buttons();
+        transparentStatusBar();
+    }
+
+    private void transparentStatusBar() {
+        // In Activity's onCreate() for instance
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 
     private void initViews() {
